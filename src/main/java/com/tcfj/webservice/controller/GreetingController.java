2183b04357b8b8774b17e600621fb8ad4acb5631
andrewlarsen514 <andrew.larse514@gmail.com>
2016-12-29 10:14:20 -0600

    change greeting

--- src/main/java/com/tcfj/webservice/controller/GreetingController.java
+++ src/main/java/com/tcfj/webservice/controller/GreetingController.java
@@ -1,8 +1,6 @@
 package com.tcfj.webservice.controller;
 
-import com.tcfj.webservice.model.Greeting;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.concurrent.atomic.AtomicLong;
@@ -16,9 +14,8 @@ public class GreetingController {
     private static final String template = "Hello, %s!";
     private final AtomicLong counter = new AtomicLong();
 
-    @RequestMapping("/greeting")
-    public Greeting greeting(@RequestParam(value="name", defaultValue="World") String name) {
-        return new Greeting(counter.incrementAndGet(),
-                String.format(template, name));
+    @RequestMapping("/greetings")
+    public String greeting() {
+        return "Hello World Change";
     }
 }
