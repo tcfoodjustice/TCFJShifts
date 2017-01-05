48022c47101a16c72929158ca856050cd6bef7c4
andrewlarsen514 <andrew.larse514@gmail.com>
2017-01-04 23:39:13 -0600

    total 180, circleci, docker, elastic beanstalk....go

--- deploy.sh
+++ deploy.sh
@@ -1,79 +1,19 @@
-#!/usr/bin/env bash
+# deploy.sh
+#! /bin/bash
 
-# more bash-friendly output for jq
-JQ="jq --raw-output --exit-status"
+SHA1=$1
 
-configure_aws_cli(){
-	aws --version
-	aws configure set default.region us-west-2
-	aws configure set default.output json
-}
+# Deploy image to Docker Hub
+docker push larse514/tcfjshifts:$SHA1
 
-deploy_cluster() {
+# Create new Elastic Beanstalk version
+EB_BUCKET=elasticbeanstalk-us-west-2-417615409974
+DOCKERRUN_FILE=$SHA1-Dockerrun.aws.json
+sed "s/<TAG>/$SHA1/" < Dockerrun.aws.json.template > $DOCKERRUN_FILE
+aws s3 cp $DOCKERRUN_FILE s3://$EB_BUCKET/$DOCKERRUN_FILE
+aws elasticbeanstalk create-application-version --application-name ShiftService \
+  --version-label $SHA1 --source-bundle S3Bucket=$EB_BUCKET,S3Key=$DOCKERRUN_FILE
 
-    family="ShiftPersistence"
-
-    make_task_def
-    register_definition
-    if [[ $(aws ecs update-service --cluster TCFJCluster --service ShiftService --task-definition $revision | \
-                   $JQ '.service.taskDefinition') != $revision ]]; then
-        echo "Error updating service."
-        return 1
-    fi
-
-    # wait for older revisions to disappear
-    # not really necessary, but nice for demos
-    for attempt in {1..300}; do
-        if stale=$(aws ecs describe-services --cluster TCFJCluster --services ShiftPersistence | \
-                       $JQ ".services[0].deployments | .[]? | select(.taskDefinition != \"$revision\") | .taskDefinition"); then
-            echo "Waiting for stale deployments:"
-            echo "$stale"
-            sleep 5
-        else
-            echo "Deployed!"
-            return 0
-        fi
-    done
-    echo "Service update took too long."
-    return 1
-}
-
-make_task_def(){
-	task_template='[
-		{
-			"name": "ShiftPersistence",
-			"image": "417615409974.dkr.ecr.us-west-2.amazonaws.com/tcfj-shiftspersistence:latest",
-			"essential": true,
-			"memory": 200,
-			"cpu": 10,
-			"portMappings": [
-				{
-					"containerPort": 8080,
-					"hostPort": 80
-				}
-			]
-		}
-	]'
-	task_def=$(printf "$task_template")
-}
-
-push_ecr_image(){
-	eval $(aws ecr get-login --region us-west-2)
-    docker push 417615409974.dkr.ecr.us-west-2.amazonaws.com/tcfj-shiftspersistence:latest
-}
-
-register_definition() {
-
-    echo "$task_def"
-    if revision=$(aws ecs register-task-definition --container-definitions "$task_def" --family $family | $JQ '.taskDefinition.taskDefinitionArn'); then
-        echo "Revision: $revision"
-    else
-        echo "Failed to register task definition"
-        return 1
-    fi
-
-}
-
-configure_aws_cli
-push_ecr_image
-deploy_cluster
+# Update Elastic Beanstalk environment to new version
+aws elasticbeanstalk update-environment --environment-name ShiftService-env \
+    --version-label $SHA1
