c82c28573967799933046c0bf8b21115a11899ec
andrewlarsen514 <andrew.larse514@gmail.com>
2017-01-07 17:45:33 -0600

    ls file system

--- deploy.sh
+++ deploy.sh
@@ -12,6 +12,7 @@ DOCKERRUN_FILE=$SHA1-Dockerrun.aws.json
 sed "s/<TAG>/$SHA1/" < Dockerrun.aws.json.template > $DOCKERRUN_FILE
 echo $DOCKERRUN_FILE
 echo $EB_BUCKET
+ls
 aws s3 cp $DOCKERRUN_FILE s3://$EB_BUCKET/$DOCKERRUN_FILE
 aws elasticbeanstalk create-application-version --application-name ShiftsService \
   --version-label $SHA1 --source-bundle S3Bucket=$EB_BUCKET,S3Key=$DOCKERRUN_FILE
