0b4a51d0509374b2b173629e6bee12811ced5736
andrewlarsen514 <andrew.larse514@gmail.com>
2017-01-07 17:10:35 -0600

    added correcct app name

--- deploy.sh
+++ deploy.sh
@@ -13,9 +13,9 @@ sed "s/<TAG>/$SHA1/" < Dockerrun.aws.json.template > $DOCKERRUN_FILE
 echo $DOCKERRUN_FILE
 echo $EB_BUCKET
 aws s3 cp $DOCKERRUN_FILE s3://$EB_BUCKET/$DOCKERRUN_FILE
-aws elasticbeanstalk create-application-version --application-name ShiftService \
+aws elasticbeanstalk create-application-version --application-name ShiftsService \
   --version-label $SHA1 --source-bundle S3Bucket=$EB_BUCKET,S3Key=$DOCKERRUN_FILE
 
 # Update Elastic Beanstalk environment to new version
-aws elasticbeanstalk update-environment --environment-name ShiftService-env \
+aws elasticbeanstalk update-environment --environment-name ShiftsService-env \
     --version-label $SHA1
