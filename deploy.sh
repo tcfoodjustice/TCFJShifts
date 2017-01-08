1c21321b07eb82bb10e819396aa573805206bb89
andrewlarsen514 <andrew.larse514@gmail.com>
2017-01-07 18:22:41 -0600

    test describe api

--- deploy.sh
+++ deploy.sh
@@ -9,10 +9,12 @@ docker push larse514/tcfjshifts:$SHA1
 # Create new Elastic Beanstalk version
 EB_BUCKET=elasticbeanstalk-us-west-2-417615409974
 DOCKERRUN_FILE=$SHA1-Dockerrun.aws.json
+
 sed "s/<TAG>/$SHA1/" < Dockerrun.aws.json.template > $DOCKERRUN_FILE
 echo $DOCKERRUN_FILE
 echo $EB_BUCKET
 ls
+
 aws s3 cp $DOCKERRUN_FILE s3://$EB_BUCKET/$DOCKERRUN_FILE
 aws elasticbeanstalk create-application-version --application-name ShiftsService \
   --version-label $SHA1 --source-bundle S3Bucket=$EB_BUCKET,S3Key=$DOCKERRUN_FILE
@@ -20,3 +22,5 @@ aws elasticbeanstalk create-application-version --application-name ShiftsService
 # Update Elastic Beanstalk environment to new version
 aws elasticbeanstalk update-environment --environment-name ShiftsService-env \
     --version-label $SHA1
+
+aws elasticbeanstalk describe-environment-managed-actions --environment-name ShiftsService-env
