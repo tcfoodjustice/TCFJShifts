13c01619f6b0220428ffde77834e7d457d249db7
andrewlarsen514 <andrew.larse514@gmail.com>
2017-01-09 22:04:09 -0600

    add a large deployment change to make deploy process synchronous

--- deploy.sh
+++ deploy.sh
@@ -16,13 +16,37 @@ echo $DOCKERRUN_FILE
 echo $EB_BUCKET
 ls
 
+#todo-update this to create environment in DEV and tear down after tests
 aws s3 cp $DOCKERRUN_FILE s3://$EB_BUCKET/$DOCKERRUN_FILE
 aws elasticbeanstalk create-application-version --application-name ShiftsService \
   --version-label $SHA1 --source-bundle S3Bucket=$EB_BUCKET,S3Key=$DOCKERRUN_FILE
 
 # Update Elastic Beanstalk environment to new version
-aws elasticbeanstalk update-environment --environment-name ShiftService-$env \
-    --version-label $SHA1
+updateResponse=`aws elasticbeanstalk update-environment --environment-name ShiftService-"${env}" \
+    --version-label "${SHA1}"`
+updateStatus = `echo $updateResponse|python -c "import sys, json; print(json.load(sys.stdin)['Status'])"`
+#check update status
+if [ $updateStatus == 'Updating' ]; then
+     echo "Update started"
+ else
+     echo "Deployment failed with status $updateStatus"
+     exit 1
+ fi
 
-aws elasticbeanstalk  describe-environment-health --environment-name ShiftService-$env \
+aws elasticbeanstalk describe-environment-health --environment-name ShiftService-$env \
     --attribute-names All
+     deploymentId=`echo $deployout|python -c "import sys, json; print(json.load(sys.stdin)['deploymentId'])"`
+
+ while [ $updateStatus != 'Ready' ] && [ $updateStatus != 'Terminated' ]; do
+     echo "deploy status is $updateStatus"
+     checkStatus = aws elasticbeanstalk describe-environment-health --environment-name ShiftService-$env \
+        --attribute-names All
+     updateStatus = `echo $checkStatus|python -c "import sys, json; print(json.load(sys.stdin)['Status'])"`
+     sleep 4
+ done
+ if [ $updateStatus == 'Ready' ]; then
+     echo "Deployment succeeded!"
+ else
+     echo "Deployment failed with status $updateStatus"
+     exit 1
+ fi
