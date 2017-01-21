f61603865b3dc2c09bd3b4ffadf5bfa584c92e64
Dr EndGame <paulhilsen@hotmail.com>
2017-01-21 15:39:07 -0600

    return a json object instead of a double for total lbs

---
+++ src/main/java/com/tcfj/webservice/dto/FoodTotalDTO.java
@@ -0,0 +1,14 @@
+package com.tcfj.webservice.dto;
+
+/**
+ * Created by Paul Hilsen on 1/21/2017.
+ */
+public class FoodTotalDTO {
+
+    private Double total;
+
+    public Double getTotal() {return total;}
+
+    public void setTotal(Double total) {this.total = total;}
+
+}
